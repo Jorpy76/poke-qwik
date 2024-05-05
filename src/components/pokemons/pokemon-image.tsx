e49be7d9f80926d1e710e87e2176ae77165e9c84
Jorge Rios <jorpy76@gmail.com>
2024-05-05 13:07:08 -0400

    end project 1

--- src/components/pokemons/pokemon-image.tsx
+++ src/components/pokemons/pokemon-image.tsx
@@ -1,16 +1,47 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useSignal, useTask$ } from '@builder.io/qwik';
 
 interface Props {
 	id: number;
 	size?: number;
+	backImage: boolean;
+	isVisible?: boolean;
 }
 
-export const PokemonImage = component$(({id, size=200 }: Props) => {
-	return (
-		<img
-			src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
-			alt='Pokemon Sprite'
-			style= {{ width: `${size}px`, height: `${size}px` }}
-		/>
-	);
-});
+export const PokemonImage = component$(
+	({ id, size = 200, backImage = false, isVisible = false }: Props) => {
+		const imageLoaded = useSignal(false);
+
+		useTask$(({ track }) => {
+			// useTask$ se utiliza para disparar efectos secundarios. track es una función que se ejecuta cuando cambia un valor.
+			track(() => id);
+			imageLoaded.value = false;
+		});
+
+		return (
+			<div
+				class='flex items-center justify-center'
+				style={{ width: `${size}px`, height: `${size}px` }}
+			>
+				{!imageLoaded.value && <span>Cargando...</span>}
+				<img
+					src={
+						backImage
+							? `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${id}.png`
+							: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
+					}
+					alt='Pokemon Sprite'
+					style={{ width: `${size}px`, height: `${size}px` }}
+					onLoad$={() => {
+						// setTimeout(() => {
+						imageLoaded.value = true;
+						// }, 1000);
+					}}
+          class={[{
+            'hidden': !imageLoaded.value,
+            'brightness-0': isVisible
+          }, 'transition-all']}
+				/>
+			</div>
+		);
+	},
+);
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,6 +6,10 @@ import { PokemonImage } from '../components/pokemons/pokemon-image';
 export default component$(() => {
 	const pokemonId = useSignal(1); // useSignal se usa para primitivos, booleans, numbers, strings useStore(); // useStore se usa para objetos
 
+	const showBackImage = useSignal(false);
+
+  const isVisibleImage = useSignal(false);
+
 	const changePokemonId = $((value: number) => {
 		if (pokemonId.value + value <= 0) return;
 		pokemonId.value += value;
@@ -18,7 +22,11 @@ export default component$(() => {
 			<span class='mt-4 text-2xl'>Buscador de pokemones</span>
 			<span class='text-6xl'>{pokemonId}</span>
 
-			<PokemonImage id={pokemonId.value} />
+			<PokemonImage
+				id={pokemonId.value}
+				backImage={showBackImage.value}
+        isVisible={isVisibleImage.value}
+			/>
 
 			<div class='mt-4'>
 				<button
@@ -31,9 +39,23 @@ export default component$(() => {
 				<button
 					type='button'
 					onClick$={() => changePokemonId(1)}
-					class='btn btn-primary '
+					class='btn btn-primary mr-4'
 				>
 					Siguiente
+				</button>
+				<button
+					type='button'
+					onClick$={() => (showBackImage.value = !showBackImage.value)}
+					class='btn btn-primary mr-4'
+				>
+					Voltear
+				</button>
+				<button
+					type='button'
+					onClick$={() => (isVisibleImage.value = !isVisibleImage.value)}
+					class='btn btn-primary mr-4'
+				>
+					Revelar
 				</button>
 			</div>
 		</>
