b55ee67b9c831f9b390fba6b397f9d0ab5cdfe31
Jorge Rios <jorpy76@gmail.com>
2024-05-11 08:39:37 -0400

    Perf properly size image error

--- src/routes/pokemons/list-client/index.tsx
+++ src/routes/pokemons/list-client/index.tsx
@@ -14,13 +14,15 @@ import { PokemonImage } from '~/components/pokemons/pokemon-image';
 
 interface PokemonPageState {
 	currentPage: number;
-	pokemons: SmallPokemon[];
+	pokemons: SmallPokemon[],
+  isLoading: boolean
 }
 
 export default component$(() => {
 	const pokemonState = useStore<PokemonPageState>({
 		currentPage: 0,
 		pokemons: [],
+    isLoading: false
 	});
 
 	// useVisibleTask$(async ({track}) => {
@@ -33,16 +35,25 @@ export default component$(() => {
 
 	useTask$(async ({ track }) => {
 		track(() => pokemonState.currentPage);
+
+    // pokemonState.isLoading = true;
+
 		const pokemons = await getSmallPokemons(pokemonState.currentPage * 10, 30);
 		pokemonState.pokemons = [...pokemonState.pokemons, ...pokemons];
+
+    pokemonState.isLoading = false;
 	});
 
-	useOnDocument(
-		'scroll',
-		$(event => {
-			console.log('scroll', event);
-		}),
-	);
+	useOnDocument('scroll',$(() => {
+    const maxScroll = document.body.scrollHeight
+    const currentScroll = window.scrollY + window.innerHeight
+  
+    if ((currentScroll + 200) >= maxScroll && !pokemonState.isLoading) {
+      pokemonState.isLoading = true
+      pokemonState.currentPage++
+    }
+
+		}));
 
 	return (
 		<>
